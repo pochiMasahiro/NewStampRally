5b012f54c7d691bf81ea473b2e0da895a82f31b7
pochiMasahiro <pochi.masahiro@gmail.com>
2015-12-08 08:25:25 +0900

    ファイル消す

--- StampRally/check-point/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// Skinnyの呼び出し、各コントローラーへの実装し直しを行う予定
-	include_once( "../Skinny.php" );
-	require_once "./dispatcher.php";
-	
-	$dispatcher = new Dispatcher();
-	$dispathcer -> dispatch();
-?>
